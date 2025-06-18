e2ae22dc89f234d6d1841c5de370b3e4472aeac7
SOSPEL <Seb1234567891011>
2025-06-18 11:32:36 -0700

    completed lab 2

---
+++ Desktop/hack the hood a/lab2.py
@@ -0,0 +1,20 @@
+# Lab 2
+
+## Step 1
+name = "Sebastian Elenes"
+print(name)
+
+## Step 2
+nickname = "Seb"
+
+## Step 3
+age = 15
+
+## Step 4
+used_python = False 
+
+## Step 5
+hobbies = [Soccer, Volleyball, games]
+
+## Step 6
+favorite_things = {"Games": "Fortnite"}

f8efc9e7d095e2b7f82c5a8cd7ea2812f3489309
SOSPEL <Seb1234567891011>
2025-06-25 12:06:36 -0700

    finished lab 3

---
+++ Desktop/hack the hood a/lab3.py
@@ -0,0 +1,29 @@
+# Lab 3
+
+
+food = "pizza with ham"
+print (food[:3])
+print (food{-3:})
+first_last = food[0] + food{-1}
+print(first_last)
+food_list = food.split()
+print (food_list)
+joined_food = '#'.join(food_list)
+print(joined_food)
+
+number_list = [45, 72385, -1, 0, -32920]
+number_list.append(789)
+print(number_list)
+number_list.insert(3, -8945)
+print(number_list)
+number_list.pop()
+print(number_list)
+number_list,remove(number_list(1))
+print(number_list)
+print(number_list[:3])
+print(number_list[-3:])
+
+books = {'Suzzane collins': 'The hunger games', 'George Orwell': '1984, 'Rick riordan': 'Percy Jackson', 'Jeff Kiney':'Diary of a Wimpy kid'}
+print(books.keys())
+print(books.values())
+print(books.get)
